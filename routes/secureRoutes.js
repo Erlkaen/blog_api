5450962eb50771f495bca6fee9d001c435c9c564
= <=>
2020-03-22 17:53:26 +0100

    added update route

--- routes/secureRoutes.js
+++ routes/secureRoutes.js
@@ -40,6 +40,40 @@ router.post('/articles', async (req, res, next) => {
   }
 })
 
+router.patch('/article/:article_title', async (req, res, next) => {
+  try {
+    let article = {
+      article_title: req.body.article_title,
+      article_body: req.body.article_body,
+      article_date: req.body.article_date,
+      article_owner: {}
+    }
+    article.article_owner = req.user.user
+    axios.get(config.DB_URL + 'blog-articles?q={"article_title":"' + req.params.article_title + '"}').then(datas => {
+      axios.patch(config.DB_URL + 'blog-articles/' + datas.data[0]._id, article).then(data => {
+        return res.json({
+          message : 'Article Successfully updated',
+          article: data.data
+        })
+      }).catch(err => {
+        return res.json({
+          message : 'Article updating failed',
+          error: err
+        })
+      })
+    }).catch(err => {
+      return res.json({
+        message: 'No article found',
+        error: err
+      })
+    })
+
+
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.delete('/article/:article_title', async (req, res, next) => {
   try {
     axios.delete(config.DB_URL + 'blog-articles/*?q={"article_title":"' + req.params.article_title + '"}').then(data => {
